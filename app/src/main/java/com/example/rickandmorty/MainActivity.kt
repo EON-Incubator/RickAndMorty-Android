61e009431570aaf97f846ed8fae63bdffbd9a285
Bakhshish Singh Dhillon <91991520+bakhshish-singh-dhillon@users.noreply.github.com>
2023-03-08 15:50:16 -0500

    Graphql setup (#3)
    * Apollo Setup Completed
    
    * Apollo initial setup completed

--- app/src/main/java/com/example/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.rickandmorty
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.example.rickandmorty.api.ApolloModule
 import com.example.rickandmorty.ui.theme.RickAndMortyTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        lifecycleScope.launch {
+            val characterClient = ApolloModule.provideGetCharactersClient(ApolloModule.provideApolloClient())
+            Log.v("Test",characterClient.getCharacters().toString());
+        }
         setContent {
             RickAndMortyTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,22 +27,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    RickAndMortyTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/rickandmorty/api/ApolloModule.kt
@@ -0,0 +1,28 @@
+package com.example.rickandmorty.api
+
+import com.apollographql.apollo3.ApolloClient
+import com.example.rickandmorty.data.ApolloCharacterClient
+import com.example.rickandmorty.domain.CharacterClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApolloModule {
+    @Provides
+    @Singleton
+    fun provideApolloClient(): ApolloClient {
+        return ApolloClient.Builder()
+            .serverUrl("https://rickandmortyapi.com/graphql")
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetCharactersClient(apolloClient: ApolloClient): CharacterClient {
+        return ApolloCharacterClient(apolloClient)
+    }
+}

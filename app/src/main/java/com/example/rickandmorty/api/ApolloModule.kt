9f386c999f96e1ae3d518da29d074f0e11786e49
Bakhshish Singh Dhillon <bakhshish.dev@gmail.com>
2023-03-08 14:21:22 -0600

    Apollo initial setup completed

--- app/src/main/java/com/example/rickandmorty/api/ApolloModule.kt
+++ app/src/main/java/com/example/rickandmorty/api/ApolloModule.kt
@@ -11,7 +11,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class ApolloModule {
+object ApolloModule {
     @Provides
     @Singleton
     fun provideApolloClient(): ApolloClient {

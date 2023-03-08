2bfff4a9c38ee76c385dadc71e903f4f9f356d33
Mansimar <mansimars00@gmail.com>
2023-03-08 10:09:28 -0600

    made a basic architecture for the app which also includes the schema for the graphql and their dependencies

---
+++ app/src/main/java/com/example/rickandmorty/ui/screens/location/LocationViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.rickandmorty.ui.screens.location
+
+import androidx.lifecycle.ViewModel
+
+class LocationViewModel: ViewModel() {
+}
---
+++ app/src/main/java/com/example/rickandmorty/ui/screens/search/SearchViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.rickandmorty.ui.screens.search
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel: ViewModel() {
+}

664382d54bae55a7d3fa7d01b75d5cb4ba566981
JMatas <jd052613@gmail.com>
2023-03-09 11:00:56 -0600

    created graphQL queries: Episodes.graphql, Episode.graphql created data class: Episodes.kt, DetailedEpisode.kt created client: EpisodeClient.kt created mapper: EpisodeMapper.kt

---
+++ app/src/main/java/com/example/rickandmorty/domain/EpisodeClient.kt
@@ -0,0 +1,6 @@
+package com.example.rickandmorty.domain
+
+interface EpisodeClient {
+    suspend fun getEpisodes(page: Int): List<Episodes>
+    suspend fun getEpisode(id: Int): DetailedEpisode?
+}
---
+++ app/src/main/java/com/example/rickandmorty/domain/Episodes.kt
@@ -0,0 +1,7 @@
+package com.example.rickandmorty.domain
+
+data class Episodes(
+    val name: String?,
+    val episode: String?,
+    val air_date: String?
+)

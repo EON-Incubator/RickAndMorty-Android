044ed573661447737b35087c70d0190446da4f0c
Mansimar <mansimars00@gmail.com>
2023-03-10 09:50:23 -0600

    added the common utils to use in project

--- app/src/main/java/com/example/rickandmorty/ui/screens/location/Location.kt
+++ app/src/main/java/com/example/rickandmorty/ui/screens/location/Location.kt
@@ -1,1 +1,48 @@
 package com.example.rickandmorty.ui.screens.location
+
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.rickandmorty.R
+import com.example.rickandmorty.ui.screens.common_utils.ScreenNameBar
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true, uiMode = UI_MODE_NIGHT_NO)
+fun LocationScreenPreviewLightMode()
+{
+    LocationScreen()
+}
+
+
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true, uiMode = UI_MODE_NIGHT_YES)
+fun LocationScreenPreviewDarkMode()
+{
+    LocationScreen()
+}
+
+
+@Composable
+fun LocationScreen()
+{
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            ScreenNameBar(name = stringResource(R.string.location), onFilterClick = {})
+        }
+    }
+
+
+}

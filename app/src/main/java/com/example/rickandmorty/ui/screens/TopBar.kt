fd3618c8b0fc1f8128da92d1e3da2c5530bdef86
Jashan <jashanpreet28122001@gmail.com>
2023-03-10 15:42:33 -0600

    formatting issues resolved

--- app/src/main/java/com/example/rickandmorty/ui/screens/TopBar.kt
+++ app/src/main/java/com/example/rickandmorty/ui/screens/TopBar.kt
@@ -1,6 +1,5 @@
 package com.example.rickandmorty.ui.screens
 
-import android.icu.text.ListFormatter.Width
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
@@ -10,7 +9,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.example.rickandmorty.R
 
 @Composable
@@ -22,7 +20,8 @@ fun RickAndMortyTopAppBar(
 ) {
 
     if (canNavigateBack) {
-        TopAppBar( modifier = modifier.fillMaxSize(),
+        TopAppBar(
+            modifier = modifier.fillMaxSize(),
             title = { TopBar(title = title) },
 
             navigationIcon = {
@@ -35,16 +34,15 @@ fun RickAndMortyTopAppBar(
             }
         )
     } else {
-        TopAppBar(title = { TopBar(title = title)}, modifier = modifier)
+        TopAppBar(title = { TopBar(title = title) }, modifier = modifier)
     }
 }
 @Composable
-fun TopBar(title: String){
+fun TopBar(title: String) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .background(MaterialTheme.colors.primary)
-        ,
+            .background(MaterialTheme.colors.primary),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
--- app/src/main/java/com/example/rickandmorty/ui/screens/character/Character.kt
+++ app/src/main/java/com/example/rickandmorty/ui/screens/character/Character.kt
@@ -1,29 +1,48 @@
 package com.example.rickandmorty.ui.screens.character
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.rickandmorty.R
 import com.example.rickandmorty.navigation.NavigationDestination
+import com.example.rickandmorty.ui.screens.commonUtils.ScreenNameBar
 
 @Composable
 fun Characters(state: CharacterViewModel.characterState) {
-    Box(modifier = Modifier.fillMaxSize()) {
+
+    Column(modifier = Modifier.fillMaxSize()) {
         if (state.isLoading) {
             CircularProgressIndicator(
-                modifier = Modifier.align(Alignment.Center)
+                modifier = Modifier
+                    .fillMaxSize()
+                    .align(Alignment.CenterHorizontally)
             )
         } else {
-            LazyColumn() {
-                items(state.characters) { item ->
-                    characterItem(item)
+            ScreenNameBar(name = "Characters", onFilterClick = {})
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2), verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalArrangement = Arrangement.Center, modifier = Modifier.padding(8.dp)
+            ) {
+                items(state.characters) { character ->
+                    characterItem(state = character)
                 }
             }
         }
@@ -37,5 +56,32 @@ object CharacterDestination : NavigationDestination {
 
 @Composable
 private fun characterItem(state: com.example.rickandmorty.domain.character.Character) {
-    AsyncImage(model = state.image, contentDescription = null)
+
+    Card(
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxSize().clip(RoundedCornerShape(12.dp)),
+        elevation = 12.dp
+    ) {
+        Box(contentAlignment = Alignment.BottomCenter) {
+
+            AsyncImage(
+                model = state.image, contentDescription = null,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(
+                        RoundedCornerShape(8.dp)
+                    ),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = state.name.toString(),
+                modifier = Modifier
+                    .background(MaterialTheme.colors.primaryVariant)
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colors.onPrimary, style = MaterialTheme.typography.body1
+            )
+        }
+    }
 }

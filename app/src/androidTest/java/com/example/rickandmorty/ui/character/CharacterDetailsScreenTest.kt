67eb8ebf1eacae4528a438b87321c1b0dc3b9d10
Jashan <jashanpreet28122001@gmail.com>
2023-03-24 09:21:15 -0500

    RMK-66-Character-screen-UI-test completed

---
+++ app/src/androidTest/java/com/example/rickandmorty/ui/character/CharacterDetailsScreenTest.kt
@@ -0,0 +1,11 @@
+package com.example.rickandmorty.ui.character
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+
+class CharacterDetailsScreenTest() {
+    @get:Rule
+    var composeTestRule = createComposeRule()
+
+    // var testState
+}

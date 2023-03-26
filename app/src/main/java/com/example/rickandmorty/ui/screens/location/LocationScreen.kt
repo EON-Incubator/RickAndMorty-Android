abe712be7f6cc911672ee50476487d63a6b65cbe
Mansimar <mansimars00@gmail.com>
2023-03-26 06:17:07 -0500

    RMK-91 Implementation: Pull Refresh Location Screen

--- app/src/main/java/com/example/rickandmorty/ui/screens/location/LocationScreen.kt
+++ app/src/main/java/com/example/rickandmorty/ui/screens/location/LocationScreen.kt
@@ -11,12 +11,20 @@ import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.*
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.rickandmorty.ui.screens.ScreenType
 import com.example.rickandmorty.ui.screens.commonUtils.GetRowWithFourImages
 import com.example.rickandmorty.ui.screens.commonUtils.GetRowWithOneImage
@@ -33,6 +41,7 @@ object LocationDestination : NavigationDestination {
 /**
  * Function that displays all the components on the LocationScreen Tab
  */
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun LocationScreen(
     locationsUiState: LocationViewModel.LocationUiState,
@@ -40,62 +49,82 @@ fun LocationScreen(
     listState: LazyGridState,
     deviceType: ScreenType,
 ) {
+    val viewModel: LocationViewModel = hiltViewModel()
+    val isRefreshing by viewModel.isRefreshing.collectAsState()
+    val pullRefreshState =
+        rememberPullRefreshState(isRefreshing, onRefresh = { viewModel.refresh() })
     Surface(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
-        Column(modifier = Modifier.fillMaxSize().semantics { contentDescription = "Locations" }) {
-            if (deviceType != ScreenType.LANDSCAPE_PHONE) {
-                ScreenNameBar(
-                    name = stringResource(R.string.location),
-                    onFilterClick = {}
-                )
-            }
-            if (locationsUiState.isLoading) {
-                LocationLoader(deviceType)
-            } else {
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(
-                        when (deviceType) {
-                            ScreenType.PORTRAIT_PHONE -> 1
-                            else -> 2
-                        }
-                    ),
-                    verticalArrangement = Arrangement.spacedBy(8.dp),
-                    horizontalArrangement = Arrangement.Center,
-                    modifier = Modifier.padding(8.dp),
-                    state = listState
-                ) {
-                    items(locationsUiState.locations) { location ->
+        Box(modifier = Modifier.pullRefresh(pullRefreshState)) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .semantics { contentDescription = "Locations" }
+            ) {
+                if (deviceType != ScreenType.LANDSCAPE_PHONE) {
+                    ScreenNameBar(
+                        name = stringResource(R.string.location),
+                        onFilterClick = {}
+                    )
+                }
+                if (locationsUiState.isLoading) {
+                    LocationLoader(deviceType)
+                } else {
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(
+                            when (deviceType) {
+                                ScreenType.PORTRAIT_PHONE -> 1
+                                else -> 2
+                            }
+                        ),
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                        horizontalArrangement = Arrangement.Center,
+                        modifier = Modifier.padding(8.dp),
+                        state = listState
+                    ) {
+                        items(locationsUiState.locations) { location ->
 
-                        // Method in CommonUtils that draws the Card with 4 Images
+                            // Method in CommonUtils that draws the Card with 4 Images
 
-                        if (deviceType == ScreenType.LANDSCAPE_PHONE) {
-                            GetRowWithOneImage(
-                                imageUrlLink =
-                                if (location.images?.isEmpty() == true) { "" } else { location.images?.get(0) }
-                                    .toString(),
-                                titleName = location.name.toString(),
-                                property1 = location.type.toString(),
-                                property2 = location.dimension.toString(),
-                                status = "",
-                                id = location.id.toString(),
-                                onClickable = onClick
-                            )
-                        } else {
-                            GetRowWithFourImages(
-                                imageUrlLink = location.images,
-                                titleName = location.name.toString(),
-                                property1 = location.type.toString(),
-                                property2 = location.dimension.toString(),
-                                onClickable =
-                                onClick,
-                                id = location.id.toString()
-                            )
+                            if (deviceType == ScreenType.LANDSCAPE_PHONE) {
+                                GetRowWithOneImage(
+                                    imageUrlLink =
+                                    if (location.images?.isEmpty() == true) {
+                                        ""
+                                    } else {
+                                        location.images?.get(0)
+                                    }
+                                        .toString(),
+                                    titleName = location.name.toString(),
+                                    property1 = location.type.toString(),
+                                    property2 = location.dimension.toString(),
+                                    status = "",
+                                    id = location.id.toString(),
+                                    onClickable = onClick
+                                )
+                            } else {
+                                GetRowWithFourImages(
+                                    imageUrlLink = location.images,
+                                    titleName = location.name.toString(),
+                                    property1 = location.type.toString(),
+                                    property2 = location.dimension.toString(),
+                                    onClickable =
+                                    onClick,
+                                    id = location.id.toString()
+                                )
+                            }
                         }
                     }
                 }
             }
+            PullRefreshIndicator(
+                refreshing = isRefreshing,
+                state = pullRefreshState,
+                modifier = Modifier.align(Alignment.TopCenter)
+            )
         }
     }
 }

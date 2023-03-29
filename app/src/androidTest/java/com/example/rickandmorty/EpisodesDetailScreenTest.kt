a5cca235add3a096d023ee7d9879633ec30a96a4
JMatas <jd052613@gmail.com>
2023-03-29 17:53:08 -0500

    > EpisodesDetailScreenTest.kt code refactored

--- app/src/androidTest/java/com/example/rickandmorty/EpisodesDetailScreenTest.kt
+++ app/src/androidTest/java/com/example/rickandmorty/EpisodesDetailScreenTest.kt
@@ -1,5 +1,3 @@
-// import androidx.compose.ui.test.junit4.onNodeWithText
-// import androidx.compose.ui.test.junit4.onNodeWithTag
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
@@ -19,103 +17,60 @@ class EpisodesDetailScreenTest {
     @Test
     fun episodesDetailScreenTest() {
         // Set up the viewmodel with some test data.
-        val viewModel = EpisodeDetailViewModel.DetailEpisodesState(
-            characters = listOf(),
-            isLoading = false,
-            selectedEpisode = DetailedEpisode(
-                "1",
-                "Pilot",
-                "S01E01",
-                "2021-01-01",
-                listOf(
-                    Character("1", "Rick", "url_1", "Human", "Alive", "Male"),
-                    Character(ID = "2", "Minty", "url_2", "Vampire", "Dead", "Female")
-                )
-            )
-        )
+        val viewModel = testDataSetUp()
 
         // Set up the composable with the viewmodel.
-        composeTestRule.setContent {
-            EpisodeDetails(
-                state = viewModel,
-                navigateUp = { },
-                onCharacterClick = { },
-                deviceType = ScreenType.PORTRAIT_PHONE
-            )
-        }
+        screen(viewModel, ScreenType.PORTRAIT_PHONE)
 
         // Delays the screen for 5 seconds to show the data passed.
         Thread.sleep(5000)
 
-        // Verify that the screen title is displayed.
-        composeTestRule.onNodeWithText("Pilot").assertIsDisplayed()
-
-        // Verify that the episode info is displayed.
-        composeTestRule.onNodeWithText("S01E01").assertIsDisplayed()
-        composeTestRule.onNodeWithText("2021-01-01").assertIsDisplayed()
-
-        // Verify that the list of characters are displayed.
-        composeTestRule.onNodeWithText("Alive").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Rick").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Male").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Human").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Dead").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Minty").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Female").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Vampire").assertIsDisplayed()
+        asserts()
     }
 
     @Test
     fun episodesDetailScreenTestLandscape() {
-        val viewModel = EpisodeDetailViewModel.DetailEpisodesState(
-            characters = listOf(),
-            isLoading = false,
-            selectedEpisode = DetailedEpisode(
-                "1",
-                "Pilot",
-                "S01E01",
-                "2021-01-01",
-                listOf(
-                    Character("1", "Rick", "url_1", "Human", "Alive", "Male"),
-                    Character(ID = "2", "Minty", "url_2", "Vampire", "Dead", "Female")
-                )
-            )
-        )
+        val viewModel = testDataSetUp()
 
         // Set up the composable with the viewmodel.
-        composeTestRule.setContent {
-            EpisodeDetails(
-                state = viewModel,
-                navigateUp = { },
-                onCharacterClick = { },
-                deviceType = ScreenType.LANDSCAPE_PHONE
-            )
-        }
+        screen(viewModel, ScreenType.LANDSCAPE_PHONE)
 
         // Delays the screen for 5 seconds to show the data passed.
         Thread.sleep(5000)
 
-        // Verify that the screen title is displayed.
-        composeTestRule.onNodeWithText("Pilot").assertIsDisplayed()
-
-        // Verify that the episode info is displayed.
-        composeTestRule.onNodeWithText("S01E01").assertIsDisplayed()
-        composeTestRule.onNodeWithText("2021-01-01").assertIsDisplayed()
-
-        // Verify that the list of characters are displayed.
-        composeTestRule.onNodeWithText("Alive").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Rick").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Male").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Human").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Dead").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Minty").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Female").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Vampire").assertIsDisplayed()
+        asserts()
     }
 
     @Test
     fun episodesDetailScreenTestTablet() {
-        val viewModel = EpisodeDetailViewModel.DetailEpisodesState(
+        // Set up the viewmodel with some test data.
+        val viewModel = testDataSetUp()
+
+        // Set up the composable with the viewmodel.
+        screen(viewModel, ScreenType.LANDSCAPE_TABLET)
+
+        // Delays the screen for 5 seconds to show the data passed.
+        Thread.sleep(5000)
+
+        asserts()
+    }
+
+    private fun screen(
+        viewModel: EpisodeDetailViewModel.DetailEpisodesState,
+        screenType: ScreenType,
+    ) {
+        composeTestRule.setContent {
+            EpisodeDetails(
+                state = viewModel,
+                navigateUp = { },
+                onCharacterClick = { },
+                deviceType = screenType
+            )
+        }
+    }
+
+    private fun testDataSetUp(): EpisodeDetailViewModel.DetailEpisodesState {
+        return EpisodeDetailViewModel.DetailEpisodesState(
             characters = listOf(),
             isLoading = false,
             selectedEpisode = DetailedEpisode(
@@ -129,20 +84,9 @@ class EpisodesDetailScreenTest {
                 )
             )
         )
+    }
 
-        // Set up the composable with the viewmodel.
-        composeTestRule.setContent {
-            EpisodeDetails(
-                state = viewModel,
-                navigateUp = { },
-                onCharacterClick = { },
-                deviceType = ScreenType.LANDSCAPE_TABLET
-            )
-        }
-
-        // Delays the screen for 5 seconds to show the data passed.
-        Thread.sleep(5000)
-
+    private fun asserts() {
         // Verify that the screen title is displayed.
         composeTestRule.onNodeWithText("Pilot").assertIsDisplayed()
 
@@ -160,26 +104,4 @@ class EpisodesDetailScreenTest {
         composeTestRule.onNodeWithText("Female").assertIsDisplayed()
         composeTestRule.onNodeWithText("Vampire").assertIsDisplayed()
     }
-
-//    fun asserts(
-//        screenTitle:
-//
-//
-//    ){
-//        composeTestRule.onNodeWithText("Pilot").assertIsDisplayed()
-//
-//        // Verify that the episode info is displayed.
-//        composeTestRule.onNodeWithText("S01E01").assertIsDisplayed()
-//        composeTestRule.onNodeWithText("2021-01-01").assertIsDisplayed()
-//
-//        // Verify that the list of characters are displayed.
-//        composeTestRule.onNodeWithText("Alive").assertIsDisplayed()
-//        composeTestRule.onNodeWithText("Rick").assertIsDisplayed()
-//        composeTestRule.onNodeWithText("Male").assertIsDisplayed()
-//        composeTestRule.onNodeWithText("Human").assertIsDisplayed()
-//        composeTestRule.onNodeWithText("Dead").assertIsDisplayed()
-//        composeTestRule.onNodeWithText("Minty").assertIsDisplayed()
-//        composeTestRule.onNodeWithText("Female").assertIsDisplayed()
-//        composeTestRule.onNodeWithText("Vampire").assertIsDisplayed()
-//    }
 }

34f6994aa97c87642abae19c5a971df922ca229d
JMatas <jd052613@gmail.com>
2023-04-03 10:39:59 -0500

    >

--- app/src/main/java/com/example/rickandmorty/ui/screens/episode/EpisodeDetails.kt
+++ app/src/main/java/com/example/rickandmorty/ui/screens/episode/EpisodeDetails.kt
@@ -1,6 +1,5 @@
 package com.example.rickandmorty.ui.screens.episode
 
-import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
